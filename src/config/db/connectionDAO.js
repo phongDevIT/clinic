834977cc1f3fe6901b383f11a01eb56de2cbfbf8
phongDevIT <nemo24112003357@gmail.com>
2023-04-11 23:00:58 +0700

    update code

--- src/config/db/connectionDAO.js
+++ src/config/db/connectionDAO.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://quanglmbluewind:thanhs2lyt@cluster0.9ohvpkx.mongodb.net/?retryWrites=true&w=majority', {
+        await mongoose.connect("mongodb://127.0.0.1:27017/clinic", {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         });
-        console.log('Connect successfully!!!');
+        console.log("Connect successfully!!!");
     } catch (error) {
         console.log(error);
-        console.log('Connect failure!!!');
+        console.log("Connect failure!!!");
     }
 }
 
