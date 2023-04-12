bb4679135ff73d90ecb4d46032b0ecd0ccad6391
phongDevIT <nemo24112003357@gmail.com>
2023-04-12 17:33:01 +0700

    fix connect db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const errorHandler = require('./middleware/error-handler');
+const cors = require("cors");
+const errorHandler = require("./middleware/error-handler");
 
-const db = require('./config/db/connectionDAO');
-
+const db = require("./config/db/connectionDAO");
 
 // Connect to DB
 db.connect();
@@ -14,22 +13,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // api routes
-app.use('/auth', require('./routes/auth.router'));
+app.use("/auth", require("./routes/auth.router"));
 //admin
-app.use('/accounts', require('./routes/account.router'));
-app.use('/doctors', require('./routes/doctor.router'));
-app.use('/servicebases', require('./routes/servicebase.routrer'));
+app.use("/accounts", require("./routes/account.router"));
+app.use("/doctors", require("./routes/doctor.router"));
+app.use("/servicebases", require("./routes/servicebase.routrer"));
 
-//dịch vụ: api getall, getbyid thì user với admin chung còn lại của admin 
-app.use('/services', require('./routes/service.router'));
+//dịch vụ: api getall, getbyid thì user với admin chung còn lại của admin
+app.use("/services", require("./routes/service.router"));
 
 //hóa đơn: api getbyid user admin dùng chung còn lại của admin
-app.use('/bills', require('./routes/bill.router'));
-app.use('/patients', require('./routes/patient.router'));
-app.use('/patientservices', require('./routes/patient_service.router'));
+app.use("/bills", require("./routes/bill.router"));
+app.use("/patients", require("./routes/patient.router"));
+app.use("/patientservices", require("./routes/patient_service.router"));
 // global error handler
 app.use(errorHandler);
 
-app.listen(3000, function() {
-    console.log('Node server running @ http://localhost:3000')
+app.listen(3001, function () {
+    console.log("Node server running @ http://localhost:3001");
 });
